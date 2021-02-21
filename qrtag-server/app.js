73e1533e9f8697856155431fc3abaaaf1e7dd731
Jasper M-W <jasper@ngita.co.nz>
2021-02-21 17:29:27 +1300

    fcuk

--- qrtag-server/app.js
+++ qrtag-server/app.js
@@ -156,7 +156,7 @@ io.on("connection", (socket, req) => {
             }));
             return;
           }
-          let otherPlayer = _.find(state.players, (val, _index, _array) => val === json["if"]);
+          let otherPlayer = _.find(state.players, (val, _index, _array) => val.playerID === json["id"]);
           console.log(_.find)
           console.log(state.players)
           console.log(otherPlayer)
